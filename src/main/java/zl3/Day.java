4b8985378b72babbd5e6e1859cc68fac576bcf0f
Krzysztof Porwol <220742689+KrzysiekPorwol@users.noreply.github.com>
2026-09-23 14:20:42 +0200

    refactor(ZL-3): Apply code review fixes
    - Move day range validation to static method in Day
    - Replace isPresent()/get() with ifPresent()

--- src/main/java/zl3/Day.java
+++ src/main/java/zl3/Day.java
@@ -3,20 +3,22 @@ package zl3;
 import java.util.Optional;
 
 public enum Day {
-  MONDAY(1, "Poniedziałek"),
-  THUESDAY(2, "Wtorek"),
-  WEDNESDAY(3, "Środa"),
-  THURSDAY(4, "Czwartek"),
-  FRIDAY(5, "Piątek"),
-  SATURDAY(6, "Sobota"),
-  SUNDAY(7, "Niedziela");
+  MONDAY(1, "Poniedziałek", false),
+  THUESDAY(2, "Wtorek", false),
+  WEDNESDAY(3, "Środa", false),
+  THURSDAY(4, "Czwartek", false),
+  FRIDAY(5, "Piątek", false),
+  SATURDAY(6, "Sobota", true),
+  SUNDAY(7, "Niedziela", true);
 
   private final int dayOfTheWeek;
   private final String description;
+  private final boolean isWeekend;
 
-  Day(int numberRepresentation, String description) {
+  Day(int numberRepresentation, String description, boolean isWeekend) {
     this.description = description;
     this.dayOfTheWeek = numberRepresentation;
+    this.isWeekend = isWeekend;
   }
 
   public int getDayOfTheWeek() {
@@ -27,6 +29,8 @@ public enum Day {
     return description;
   }
 
+  public boolean isWeekend() { return isWeekend; }
+
   public static Optional<Day> getByDayOFTheWeek(int number) {
     for (Day d : Day.values()) {
       if (number == d.getDayOfTheWeek()) {
@@ -36,15 +40,11 @@ public enum Day {
     return Optional.empty();
   }
 
-
-
-  public boolean isWeekend() {
-    if (dayOfTheWeek == SATURDAY.dayOfTheWeek || dayOfTheWeek == SUNDAY.dayOfTheWeek) {
+  public static boolean isDayOutOfRange(int day) {
+    if (day > Day.SUNDAY.getDayOfTheWeek() || day < Day.MONDAY.getDayOfTheWeek()) {
+      System.out.println("Liczba spoza zakresu");
       return true;
-    } else {
-      return false;
     }
+    return false;
   }
-
-
 }
--- src/main/java/zl3/Timetable.java
+++ src/main/java/zl3/Timetable.java
@@ -7,63 +7,54 @@ public class Timetable {
 
   public static void main(String[] args) {
 
-    Scanner input = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
 
     boolean userDayOfTheWeekIsIncorrect = true;
 
     do {
       try {
         System.out.println("Podaj który dzień tygodnia chcesz wyświetlić: (1 - 7)");
-        int day = input.nextInt();
+        int day = scanner.nextInt();
 
-        // Początkowo tutaj chciałem dać input.nextline żeby wyczyścić bufor, ale skoro tylko raz
-        // pytamy o dane od użytkownika, to bezpieczniej wydaje mi się tutaj od razu
-        // zamknąć scannera.
-        input.close();
+        userDayOfTheWeekIsIncorrect = Day.isDayOutOfRange(day);
 
-        if (day > Day.SUNDAY.getDayOfTheWeek() || day < Day.MONDAY.getDayOfTheWeek()) {
-          System.out.println("Liczba spoza zakresu");
-          continue;
-        }
-
-
-        // Tutaj niestety zrobiłem antypattern, czyli zrobilem isPresent a w nim get (Optional).
-        if (Day.getByDayOFTheWeek(day).isPresent()) {
-
-          Day chosenDay = Day.getByDayOFTheWeek(day).get();
-
-          switch (chosenDay) {
-            case MONDAY, THUESDAY, WEDNESDAY, THURSDAY, FRIDAY -> {
-              printDayInfo(chosenDay);
-              System.out.println("Godzina odjazdu: 5:30");
-            }
-            case SATURDAY -> {
-              printDayInfo(chosenDay);
-              System.out.println("Godzina odjazdu: 7:00");
-            }
-            case SUNDAY -> {
-              printDayInfo(chosenDay);
-              System.out.println("Godzina odjazdu: 9:00");
-            }
-          }
-          userDayOfTheWeekIsIncorrect = false;
-        }
+        Day.getByDayOFTheWeek(day).ifPresent(
+              chosenDay -> {
+                switch (chosenDay) {
+                  case MONDAY, THUESDAY, WEDNESDAY, THURSDAY, FRIDAY -> {
+                    printDayInfo(chosenDay);
+                    System.out.println("Godzina odjazdu: 5:30");
+                  }
+                  case SATURDAY -> {
+                    printDayInfo(chosenDay);
+                    System.out.println("Godzina odjazdu: 7:00");
+                  }
+                  case SUNDAY -> {
+                    printDayInfo(chosenDay);
+                    System.out.println("Godzina odjazdu: 9:00");
+                  }
+                }
+              }
+        );
 
       } catch (InputMismatchException e) {
-        input.nextLine();
+        scanner.nextLine();
         System.out.println("Nie wprowadziłeś liczby całkowitej.");
       }
 
     } while (userDayOfTheWeekIsIncorrect);
+
+    scanner.close();
+
   }
 
   public static void printDayInfo(Day day) {
     System.out.println(day.getDescription());
-    System.out.println(printWeekend(day.isWeekend()));
+    System.out.println(printWeekend(day));
   }
 
-  public static String printWeekend(boolean isWeekend) {
-    return isWeekend ? "Wybrany dzień to dzień weekendowy" : "Wybrany dzień to dzień roboczy";
+  public static String printWeekend(Day day) {
+    return day.isWeekend() ? "Wybrany dzień to dzien weekendowy" : "Wybrany dzień to dzień roboczy";
   }
 }
 
